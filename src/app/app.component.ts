60e6d9fde3c1549860a9c6df92d98a5e1c2c7067
mamapisouza <Pi204326@>
2024-01-16 11:54:25 -0300

    novas implementações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cadastro-web';
+
+  constructor(private formBuilder: FormBuilder){
+  }
+
+  value = 24;
+
+  formulario: FormGroup = this.formBuilder.group({
+    nome: ['', [Validators.required, Validators.minLength(1)]],
+    sobrenome: ['', [Validators.required, Validators.minLength(1)]],
+    cpf: ['', [Validators.required, Validators.minLength(1)]],
+    meses: [24, [Validators.required, Validators.minLength(1)]],
+  })
 }
